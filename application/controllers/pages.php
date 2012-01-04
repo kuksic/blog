6e46f400bf550066cb0aae2a235e035b57c9336e
kuksic <kuksic@gmail.com>
2012-01-04 15:29:47 +0100

    nov commit

---
+++ application/controllers/pages.php
@@ -0,0 +1,27 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+	class Pages extends CI_Controller {
+		
+		// function __construct(){
+		// parent::__construct();
+		// $this->load->view('template/header');
+		// }
+		
+		public function view($page = 'home')
+		{
+					
+			if ( ! file_exists('application/views/pages/'.$page.'.php'))
+			{
+				// Whoops, we don't have a page for that!
+				show_404();
+			}
+			
+			$data['title'] = ucfirst($page); // Capitalize the first letter
+			
+			$this->load->view('templates/header', $data);
+			$this->load->view('pages/'.$page, $data);
+			$this->load->view('templates/footer', $data);
+		
+		}
+	}
